e0632099f614d92543408841c2cff38a95091c70
MHW <stayhappyalltime@gmai.com>
2026-06-01 11:32:20 +0530

    Wrap WSGI app in try-except to show startup errors on Vercel

--- helpinghand/wsgi.py
+++ helpinghand/wsgi.py
@@ -7,5 +7,14 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helpinghand.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception as e:
+    import traceback
+    def application(environ, start_response):
+        status = '500 Internal Server Error'
+        headers = [('Content-type', 'text/plain; charset=utf-8')]
+        start_response(status, headers)
+        return [traceback.format_exc().encode('utf-8')]
+
 app = application  # Required by Vercel serverless functions
